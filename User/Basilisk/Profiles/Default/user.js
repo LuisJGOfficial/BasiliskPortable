1c4280368cb8b05fbbb4d42553f4fc4d1dd09607
LuisJG <145312410+LuisJGOfficial@users.noreply.github.com>
2025-01-09 12:24:57 -0500

    Add files via upload

---
+++ User/Basilisk/Profiles/Default/user.js
@@ -0,0 +1,4 @@
+user_pref("browser.cache.disk.parent_directory", "C:\\Users\\Luis\\Documents\\Basilisk\\User\\Basilisk\\Profiles\\Default");
+user_pref("browser.download.lastDir", "C:\\Users\\Luis\\Documents\\Basilisk\\Downloads");
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.taskbar.lists.enabled", false);
